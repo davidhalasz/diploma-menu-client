3e7fddb202e0c2dbc8e2200ef385b779f44eed3d
davidhalasz <38536685+davidhalasz@users.noreply.github.com>
2022-02-25 16:25:58 +0100

    refactor

--- src/common/Constants.js
+++ src/common/Constants.js
@@ -1,4 +1,5 @@
 export const BASE_API_URL =
   process.env.VUE_APP_BASE_URL || "http://localhost:8080";
 
-export const BASE_CLIENT_URL = process.env.NODE_ENV || "http://localhost:4000";
+export const BASE_CLIENT_URL =
+  process.env.VUE_APP_CLIENT_URL || "http://localhost:4000";
